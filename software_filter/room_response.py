2affcefc57cf21cb00c3699301d0aea67ac8ba00
Dawson J. Gullickson <dawsongullickson02@gmail.com>
2023-04-30 20:01:47 -0600

    Added basic room response plot

---
+++ software_filter/room_response.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+import scipy.fft as fft
+import scipy.io.wavfile as wavfile
+from scipy.constants import pi
+
+# Read text file into np array
+freqs = []
+spls = []
+with open("software_filter/AvgFreqResponse.txt") as file:
+    # Ignore first 14 lines
+    for _ in range(14): _ = file.readline()
+
+    # Fill arrays
+    while line := file.readline():
+        freq, spl = line.split(sep=None)
+        freqs.append(float(freq))
+        spls.append(float(spl))
+freqs = np.array(freqs, dtype=np.float32)
+spls = np.array(spls, dtype=np.float32)
+
+# Plot room response
+plt.figure()
+plt.title("Room Frequency Response")
+plt.xlabel("SPL (dB)")
+plt.ylabel("$f$ (Hz)")
+plt.plot(freqs, spls)
+plt.grid()
+plt.show()
